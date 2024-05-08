628a1c17345e7d2882623ae1e6012c09cd34bd0f
ajith-kallar <gost2noob@gmail.com>
2024-05-08 14:39:19 +0530

    content not added.

--- main.js
+++ main.js
@@ -1,39 +1,9 @@
-import KeenSlider from "https://cdn.jsdelivr.net/npm/keen-slider@6.8.6/+esm";
-DocumentType="module";
-const keenSlider = new KeenSlider(
-  "#keen-slider",
-  {
-    loop: true,
-    slides: {
-      origin: "center",
-      perView: 1.25,
-      spacing: 16,
-    },
-    breakpoints: {
-      "(min-width: 1024px)": {
-        slides: {
-          origin: "auto",
-          perView: 1.5,
-          spacing: 32,
-        },
-      },
-    },
-  },
-  []
-);
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("navbar-toggle");
+  const menu = document.getElementById("navbar-dropdown");
 
-const keenSliderPrevious = document.getElementById("keen-slider-previous");
-const keenSliderNext = document.getElementById("keen-slider-next");
 
-const keenSliderPreviousDesktop = document.getElementById(
-  "keen-slider-previous-desktop"
-);
-const keenSliderNextDesktop = document.getElementById(
-  "keen-slider-next-desktop"
-);
-
-keenSliderPrevious.addEventListener("click", () => keenSlider.prev());
-keenSliderNext.addEventListener("click", () => keenSlider.next());
-
-keenSliderPreviousDesktop.addEventListener("click", () => keenSlider.prev());
-keenSliderNextDesktop.addEventListener("click", () => keenSlider.next());
+  button.addEventListener("click", function () {
+    menu.classList.toggle("hidden");
+  });
+});
